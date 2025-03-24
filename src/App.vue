2f248364854ce4ba568c569284a44e5ea87fb7b7
25-masik-52 <Max2827@yandex.ru>
2025-03-24 20:01:38 +0300

    feat!: add first code

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+const title = ref('Мой первый сайт на Vue!')
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <p>Скоро этот сайт будет на GitHub Pages!</p>
+</template>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
